4cc5b03881f183bfe1b89cf9ee3c3c70bd3c3b7d
Brennan Morris <brennan.l.morris@gmail.com>
2024-12-31 17:15:46 -0500

    Registered flask blueprint in app/init.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,10 @@
 from flask import Flask
+from .routes.main import main
 
 def create_app():
     app = Flask(__name__)
 
     #Registers blueprints and configure app here
+    app.register_blueprint(main)
 
     return app
